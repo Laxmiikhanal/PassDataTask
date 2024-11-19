054cdc9a04369dfe1e3eab4b59d38b3a37b871af
Laxmi <pujakhanal674@gmail.com>
2024-11-19 22:25:47 +0545

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/RadioActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RadioButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RadioActivity : AppCompatActivity() {
+    lateinit var radiograpes : RadioButton
+    lateinit var radiocherry : RadioButton
+    lateinit var radiotulip : RadioButton
+    lateinit var imageView: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_radio)
+
+        radiograpes = findViewById(R.id.btngrapes)
+        radiocherry = findViewById(R.id.btncherry)
+        radiotulip = findViewById(R.id.btntulip)
+        imageView = findViewById(R.id.imageView2)
+
+        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
